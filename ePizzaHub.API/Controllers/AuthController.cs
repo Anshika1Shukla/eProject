91a98f84eb4d2f9f04476e258a86a2fb95f754bd
Anshika1Shukla <anshikashuklainfo@gmail.com>
2025-06-24 23:18:21 +0530

    Add Model and user

---
+++ ePizzaHub.API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ePizzaHub.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ePizzaHub.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace ePizzaHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //public IActionResult Create([FromBody]CreateUserRequest userRequest)
+
+    }
+}
